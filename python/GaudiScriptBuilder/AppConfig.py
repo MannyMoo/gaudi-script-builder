f37ef6f3a6dfbddd4b30198618903dd3e6adf84c
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2015-11-25 17:59:14 +0000

    Tweaking AppConfig to give more versatile reproduction of Configurables in output scripts.

--- python/GaudiScriptBuilder/AppConfig.py
+++ python/GaudiScriptBuilder/AppConfig.py
@@ -6,20 +6,21 @@ import Configurables
 import subprocess
 from GaudiScriptBuilder.DecayDescriptors import *
 
-def get_all_configurables(obj) :
+def get_all_configurables(obj, recursive = True, top = True) :
     configurables = set()
     if isinstance(obj, Configurable) :
         configurables.add(obj)
-        for attr, prop in obj.get_user_defined_properties().iteritems() :
-            configurables.update(get_all_configurables(prop))
+        if recursive or top :
+            for prop in obj.get_user_defined_properties().values() + obj.getTools() :
+                configurables.update(get_all_configurables(prop, recursive = recursive, top = False))
     elif isinstance(obj, (tuple, list, set)) :
         for thing in obj :
             if isinstance(thing, Configurable) :
-                configurables.update(thing.get_configurables())
+                configurables.update(thing.get_configurables(recursive = recursive, top = False))
     elif isinstance(obj, dict) :
         for thing in obj.values() :
             if isinstance(thing, Configurable) :
-                configurables.update(thing.get_configurables())
+                configurables.update(thing.get_configurables(recursive = recursive, top = False))
     return configurables 
 
 def get_all_import_lines(obj) :
@@ -31,6 +32,7 @@ def get_all_import_lines(obj) :
 
 def duck_punch_configurable() :
     def get_user_defined_properties(self) :
+        #return self.getValuedProperties()
         props = self.properties()
         defaultProps = self.getDefaultProperties()
         return dict((attr, val) for attr, val in props.iteritems() \
@@ -41,8 +43,8 @@ def duck_punch_configurable() :
             return 'from Configurables import ' + self.__class__.__name__ + '\n'
         return 'from ' + self.__module__ + ' import ' + self.__class__.__name__ + '\n'
 
-    def get_configurables(self) :
-        return get_all_configurables(self)
+    def get_configurables(self, recursive = True, top = True) :
+        return get_all_configurables(self, recursive, top)
 
     def operate_configurables(self, opr, returnMethod = None) :
         configurables = self.get_configurables()
@@ -54,15 +56,98 @@ def duck_punch_configurable() :
     def get_import_lines(self) :
         return self.operate_configurables(lambda conf : conf.get_import_line(), set)
 
-    def __repr__(self) :
-        line = self.__class__.__name__ + '(' + repr(self.getName())
-        for attr, prop in self.get_user_defined_properties().iteritems() :
-            line += ', ' + attr + ' = ' + repr(prop)
-        line += ')'
+    def get_constructor_args(self) :
+        props =  self.get_user_defined_properties()
+        # For DecayTreeTuple and TupleTools - any tools that've been added
+        # using addTupleTool will be done in the same way later.
+        if props.has_key('ToolList') :
+            toolNames = [tool.get_own_name() for tool in self.getTools()]
+            props['ToolList'] = filter(lambda toolName : toolName.split('/')[-1] not in toolNames,
+                                       props['ToolList'])
+            #if len(props['ToolList']) == 0 :
+            #    del props['ToolList']
+        return props
+
+    def to_string(self, ownVarName = None) :
+        if not Configurable._reprTop and not Configurable._reprFull :
+            return self.get_init_string()
+        reprTopOrig = Configurable._reprTop
+        # This isn't thread safe, but for generating scripts that shouldn't be
+        # a problem. You can just use difference python instances if you want
+        # to multi-task it. 
+        if Configurable._reprTop and not Configurable._reprFull :
+            Configurable._reprTop = False
+
+        if not ownVarName :
+            nameLen = len(self.__class__.__name__) + 1
+            line = self.__class__.__name__ + '(' + repr(self.get_init_name())
+            for attr, prop in self.get_constructor_args().iteritems() :
+                line += ',\n' + (' ' * nameLen) + attr + ' = ' + repr(prop)
+            line += ')'
+        else :
+            line = ''
+            for attr, prop in self.get_constructor_args().iteritems() :
+                line += ownVarName + '.' + attr + ' = ' + repr(prop) + '\n'
+            
+        if reprTopOrig and not Configurable._reprFull :
+            Configurable._reprTop = True
         return line
+            
+    def __repr__(self) :
+        return self.to_string()
+
+    def get_own_name(self) :
+        return self.getName().split('.')[-1]
+
+    def get_init_name(self) :
+        name = self.getName()
+        splitName = name.split('.')
+        if splitName[-1] != self.__class__.__name__ :
+            return splitName[-1]
+        if len(splitName) > 2 :
+            return splitName[0] + '.' + '_'.join(splitName[1:])
+        return name
+
+    def get_init_string(self) :
+        return self.__class__.__name__ + '(' + repr(self.get_init_name()) + ')'
+
+    def get_configuration_lines(self, ownVarName = None) :
+        lines = ''
+        topConfigs = self.get_configurables(False) 
+        for conf in topConfigs :
+            if conf is self :
+                continue 
+            lines += conf.get_configuration_lines()
+        lines += self.get_own_configuration_lines(ownVarName)
+        return lines
+
+    def get_tools_to_add(self) :
+        return self.getTools()
+
+    def get_own_configuration_lines(self, ownVarName = None) :
+        lines = self.to_string(ownVarName) + '\n'
+        if not ownVarName :
+            ownVarName = self.get_init_string()
+        for tool in self.get_tools_to_add() :
+            if 'Tuple' in tool.__class__.__name__ :
+                lines += ownVarName + '.addTupleTool(' \
+                    + tool.get_init_string() + ')\n'
+            else :
+                lines += ownVarName + '.addTool(' + tool.get_init_string() + ')\n'
+        lines += '\n'
+        return lines
+
+    def get_tool(self, name) :
+        for tool in self.getTools() :
+            if (name == tool.getName() or name == tool.get_own_name() 
+                or name == tool.__class__.__name__) :
+                return tool
+        return None
 
     for attr, func in locals().iteritems() :
         setattr(Configurable, attr, func)
+    Configurable._reprTop = True
+    Configurable._reprFull = False
 
     def dtt_get_import_line(self) :
         return 'from DecayTreeTuple.Configuration import *\n'
@@ -71,6 +156,48 @@ def duck_punch_configurable() :
     
     for cls, func in specialImports.iteritems() :
         cls.get_import_line = func
+
+    # Special implementations for DecayTreeTuple
+
+    def dtt_get_configurables(self, recursive = True, top = True) :
+        confs = Configurable.get_configurables(self, recursive, top)
+        # Exclude TupleToolDecay instances that represent branches
+        # - they're treated separately.
+        if hasattr(self, 'Branches') :
+            branchNames = self.Branches.keys()
+            confs = filter(lambda conf : conf.get_own_name() not in branchNames, confs)
+        return confs
+    
+    def dtt_get_tools_to_add(self) :
+        tools = self.getTools()
+        if hasattr(self, 'Branches') :
+            branchNames = self.Branches.keys()
+            tools = filter(lambda tool : tool.get_own_name() not in branchNames, tools)
+        return tools
+
+    def dtt_get_own_configuration_lines(self, ownVarName = None) :
+        lines = Configurable.get_own_configuration_lines(self, ownVarName)
+        # Extra bits to configure branches.
+        if hasattr(self, 'Branches') :
+            lines += self.get_init_string() + '.addBranches({0!r})\n\n'.format(self.Branches)
+            branchNames = self.Branches.keys()
+            branchConfigs = [getattr(self, branchName) for branchName in branchNames]
+            for branch in branchConfigs :
+                initName = self.get_init_string() + '.' + branch.get_own_name()
+                lines += branch.get_configuration_lines(initName)
+
+        return lines
+
+    def dtt_get_constructor_args(self) :
+        props = Configurable.get_constructor_args(self)
+        if props.has_key('Branches') :
+            del props['Branches']
+        return props
+
+    DecayTreeTuple.get_configurables = dtt_get_configurables
+    DecayTreeTuple.get_tools_to_add = dtt_get_tools_to_add
+    DecayTreeTuple.get_own_configuration_lines = dtt_get_own_configuration_lines
+    DecayTreeTuple.get_constructor_args = dtt_get_constructor_args
 
 duck_punch_configurable()
 
@@ -102,8 +229,10 @@ print '***'
         print stderrLines
         raise
 
+def is_trigger(version) :
+    return isinstance(version, int) or '0x' in version
 def get_line_opts(dataopts, version, linename) :
-    isTrigger = isinstance(version, int) or '0x' in version
+    isTrigger = is_trigger(version)
     opts = dataopts 
     opts += '''version = {0!r}
 linename = {1!r}
@@ -164,12 +293,12 @@ class Script(object) :
         imports = set()
         lines = []
         for obj in self.objs :
-            objline = repr(obj)
             imports.update(get_all_import_lines(obj))
-            lines.append(objline + '\n\n')
+            lines.append(objline = obj.get_configuration_lines())
         for key, obj in self.namedObjs.iteritems() :
-            objline = repr(obj)
             imports.update(get_all_import_lines(obj))
+            lines.append(obj.get_configuration_lines())
+            objline = obj.get_init_string()
             lines.append(key + ' = ' + objline + '\n\n')
         return ''.join(list(imports) + ['\n'] + lines)
 
@@ -188,9 +317,17 @@ class DaVinciScript(Script) :
                              "TupleToolEventInfo",
                              "TupleToolPrimaries",
                              "TupleToolPid",
+                             "TupleToolANNPID",
                              "TupleToolTrackInfo",
-                             "TupleToolRecoStats",]) :
-        from Configurables import GaudiSequencer, DaVinci
+                             "TupleToolRecoStats",],
+                 mcToolList = ['TupleToolMCTruth',
+                               'TupleToolMCBackgroundInfo'],
+                 L0List = [],
+                 HLT1List = [],
+                 HLT2List = [],
+                 strippingList = []) :
+        from Configurables import GaudiSequencer, DaVinci, TupleToolStripping, \
+            TupleToolTrigger
     
         # Defines Simulation, CondDBtag, DDDBtag, inputType
         dataopts = get_data_opts(datafile, explicitTags)
@@ -217,41 +354,88 @@ class DaVinciScript(Script) :
             from Configurables import TrackScaleState
             dv.UserAlgorithms.append(TrackScaleState())
 
+        isTrigger = is_trigger(version)
+        for trigList in L0List, HLT1List, HLT2List, strippingList :
+            for i, trig in enumerate(trigList) :
+                if trig[-8:] != 'Decision' :
+                    trigList[i] += 'Decision'
+
         dtts = []
         for desc in decayDescs :
-            dtt = DecayTreeTuple(descriptor_to_name(desc) + 'Tuple')
+            dtt = DecayTreeTuple(descriptor_to_name(desc) + 'Tuple',
+                                 ToolList = [])
             dtts.append(dtt)
             dtt.Decay = add_carets(desc)
             dtt.Inputs = [inputLocation]
-            dtt.ToolList = [
-                #"TupleToolTISTOS",
-                #"TupleToolTrigger",
-                "TupleToolPropertime",
-                "TupleToolKinematic",
-                "TupleToolGeometry",
-                "TupleToolEventInfo",
-                "TupleToolPrimaries",
-                "TupleToolPid",
-                "TupleToolTrackInfo",
-                "TupleToolRecoStats",
-                ]
+            for tool in toolList :
+                dtt.addTupleTool(tool)
             if dv.getProp('Simulation') :
-                dtt.ToolList += ['TupleToolMCTruth',
-                                 'TupleToolMCBackgroundInfo']
+                for tool in mcToolList :
+                    dtt.addTupleTool(tool)
+            dtt.addBranches({'lab0' : desc})
+            if not isTrigger :
+                ttstrip = dtt.lab0.addTupleTool('TupleToolStripping')
+                ttstrip.TriggerList = [linename + 'Decision'] + strippingList
+                ttstrip.VerboseStripping = True
+            else :
+                HLT2List.append(linename + 'Decision')
+            if L0List or HLT1List or HLT2List :
+                ttrig = dtt.lab0.addTupleTool('TupleToolTISTOS')
+                ttrig.TriggerList = L0List + HLT1List + HLT2List
+                ttrig.Verbose = True
+                ttrig.VerboseL0 = True
+                ttrig.VerboseHlt1 = True
+                ttrig.VerboseHlt2 = True
             dv.UserAlgorithms.append(dtt)
+
         objs = []
         if locals().has_key('CondDB') :
             objs.append(CondDB())
         objsdict = {'dv' : dv}
-        if len(dtts) == 1 :
-            objsdict['dtt'] = dtts[0]
-        else :
-            objsdict['dtts'] = dtts
+
         Script.__init__(self, fname, objs, objsdict)
         
 if __name__ == '__main__' :
 
+    from Configurables import TupleToolStripping
+    
     datafile = '/afs/cern.ch/work/m/malexand//charm/2011/data/mc/MC_2011_27163003_Beam3500GeV2011MagDownNu2Pythia8_Sim08a_Digi13_Trig0x40760037_Reco14a_Stripping20r1NoPrescalingFlagged_ALLSTREAMS.DST.py'
     opts = DaVinciScript('test.py', '20r1', 'StrippingD2hhPromptDst2D2RSLine',
-                         datafile)
+                         datafile, 
+                         L0List = ['L0' + t for t in ['Muon',
+                                                      'DiMuon',
+                                                      'Hadron',
+                                                      'MuonHigh',
+                                                      'Electron',
+                                                      'Photon'
+                                                      ]],
+                         HLT1List = ['Hlt1SingleHadron',
+                                     'Hlt1DiHadron',
+                                     'Hlt1TrackAllL0',
+                                     'Hlt1TrackMuon'],
+                         HLT2List = ['Hlt2Topo2BodySimple',
+                                     'Hlt2Topo3BodySimple',
+                                     'Hlt2Topo4BodySimple',
+                                     'Hlt2Topo2BodyBBDT',
+                                     'Hlt2Topo3BodyBBDT',
+                                     'Hlt2Topo4BodyBBDT',
+                                     'Hlt2TopoMu2BodyBBDT',
+                                     'Hlt2TopoMu3BodyBBDT',
+                                     'Hlt2TopoMu4BodyBBDT',
+                                     'Hlt2TopoE2BodyBBDT',
+                                     'Hlt2TopoE3BodyBBDT',
+                                     'Hlt2TopoE4BodyBBDT',
+                                     'Hlt2TopoRad2BodyBBDT',
+                                     'Hlt2TopoRad2plus1BodyBBDT',
+                                     'Hlt2IncPhi',
+                                     'Hlt2IncPhiSidebands'])
+
+    dtt = opts.namedObjs['dv'].UserAlgorithms[0]
+
+    ttmc = dtt.get_tool('TupleToolMCTruth')
+    #ttmc.ToolList += ['MCTupleToolPrompt']
+    from Configurables import MCTupleToolPrompt
+    ttmc.addTupleTool(MCTupleToolPrompt('mctt_prompt'))
+    ttmc.ToolList += ['MCTupleToolHierarchy']
+
     opts.write()
--- python/GaudiScriptBuilder/test.py
+++ python/GaudiScriptBuilder/test.py
@@ -1,6 +1,88 @@
-from DecayTreeTuple.Configuration import *
+from Configurables import TupleToolKinematic
+from Configurables import TupleToolMCTruth
+from Configurables import MCTupleToolPrompt
+from Configurables import TupleToolTISTOS
+from Configurables import TupleToolRecoStats
 from Configurables import DaVinci
-from Configurables import GaudiSequencer
+from Configurables import TupleToolGeometry
+from DecayTreeTuple.Configuration import *
+from Configurables import TupleToolStripping
+from Configurables import TupleToolTrackInfo
+from Configurables import TupleToolANNPID
+from Configurables import TupleToolEventInfo
+from Configurables import TupleToolMCBackgroundInfo
+from Configurables import TupleToolPid
+from Configurables import TupleToolPropertime
+from Configurables import TupleToolPrimaries
 
-dv = DaVinci('DaVinci', UserAlgorithms = [GaudiSequencer('spamseq', Members = [DecayTreeTuple('spam')])])
-dtt = DecayTreeTuple('spam')
+TupleToolPropertime('Dst2010ToD0ToKpipiTuple.TupleToolPropertime')
+
+TupleToolGeometry('Dst2010ToD0ToKpipiTuple.TupleToolGeometry')
+
+TupleToolANNPID('Dst2010ToD0ToKpipiTuple.TupleToolANNPID')
+
+TupleToolRecoStats('Dst2010ToD0ToKpipiTuple.TupleToolRecoStats')
+
+TupleToolEventInfo('Dst2010ToD0ToKpipiTuple.TupleToolEventInfo')
+
+TupleToolKinematic('Dst2010ToD0ToKpipiTuple.TupleToolKinematic')
+
+TupleToolTrackInfo('Dst2010ToD0ToKpipiTuple.TupleToolTrackInfo')
+
+MCTupleToolPrompt('mctt_prompt')
+
+TupleToolMCTruth('Dst2010ToD0ToKpipiTuple.TupleToolMCTruth',
+                 ToolList = ['MCTupleToolKinematic', 'MCTupleToolHierarchy'])
+TupleToolMCTruth('Dst2010ToD0ToKpipiTuple.TupleToolMCTruth').addTupleTool(MCTupleToolPrompt('mctt_prompt'))
+
+TupleToolMCBackgroundInfo('Dst2010ToD0ToKpipiTuple.TupleToolMCBackgroundInfo')
+
+TupleToolPrimaries('Dst2010ToD0ToKpipiTuple.TupleToolPrimaries')
+
+TupleToolPid('Dst2010ToD0ToKpipiTuple.TupleToolPid')
+
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple',
+               Inputs = ['/Event/AllStreams/Phys/D2hhPromptDst2D2RSLine/Particles'],
+               ToolList = [],
+               Decay = '[D*(2010)+ -> ^(D0 -> ^K- ^pi+) ^pi+]CC')
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolPrimaries('Dst2010ToD0ToKpipiTuple.TupleToolPrimaries'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolPid('Dst2010ToD0ToKpipiTuple.TupleToolPid'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolGeometry('Dst2010ToD0ToKpipiTuple.TupleToolGeometry'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolANNPID('Dst2010ToD0ToKpipiTuple.TupleToolANNPID'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolMCBackgroundInfo('Dst2010ToD0ToKpipiTuple.TupleToolMCBackgroundInfo'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolMCTruth('Dst2010ToD0ToKpipiTuple.TupleToolMCTruth'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolRecoStats('Dst2010ToD0ToKpipiTuple.TupleToolRecoStats'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolTrackInfo('Dst2010ToD0ToKpipiTuple.TupleToolTrackInfo'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolPropertime('Dst2010ToD0ToKpipiTuple.TupleToolPropertime'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolKinematic('Dst2010ToD0ToKpipiTuple.TupleToolKinematic'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addTupleTool(TupleToolEventInfo('Dst2010ToD0ToKpipiTuple.TupleToolEventInfo'))
+
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').addBranches({'lab0': '[D*(2010)+ -> (D0 -> K- pi+) pi+]CC'})
+
+TupleToolTISTOS('Dst2010ToD0ToKpipiTuple.lab0_TupleToolTISTOS',
+                Verbose = True,
+                VerboseHlt1 = True,
+                VerboseHlt2 = True,
+                TriggerList = ['L0MuonDecision', 'L0DiMuonDecision', 'L0HadronDecision', 'L0MuonHighDecision', 'L0ElectronDecision', 'L0PhotonDecision', 'Hlt1SingleHadronDecision', 'Hlt1DiHadronDecision', 'Hlt1TrackAllL0Decision', 'Hlt1TrackMuonDecision', 'Hlt2Topo2BodySimpleDecision', 'Hlt2Topo3BodySimpleDecision', 'Hlt2Topo4BodySimpleDecision', 'Hlt2Topo2BodyBBDTDecision', 'Hlt2Topo3BodyBBDTDecision', 'Hlt2Topo4BodyBBDTDecision', 'Hlt2TopoMu2BodyBBDTDecision', 'Hlt2TopoMu3BodyBBDTDecision', 'Hlt2TopoMu4BodyBBDTDecision', 'Hlt2TopoE2BodyBBDTDecision', 'Hlt2TopoE3BodyBBDTDecision', 'Hlt2TopoE4BodyBBDTDecision', 'Hlt2TopoRad2BodyBBDTDecision', 'Hlt2TopoRad2plus1BodyBBDTDecision', 'Hlt2IncPhiDecision', 'Hlt2IncPhiSidebandsDecision'],
+                VerboseL0 = True)
+
+TupleToolStripping('Dst2010ToD0ToKpipiTuple.lab0_TupleToolStripping',
+                   TriggerList = ['StrippingD2hhPromptDst2D2RSLineDecision'])
+
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').lab0.ToolList = []
+
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').lab0.addTupleTool(TupleToolStripping('Dst2010ToD0ToKpipiTuple.lab0_TupleToolStripping'))
+DecayTreeTuple('Dst2010ToD0ToKpipiTuple').lab0.addTupleTool(TupleToolTISTOS('Dst2010ToD0ToKpipiTuple.lab0_TupleToolTISTOS'))
+
+DaVinci('DaVinci',
+        DataType = '2011',
+        TupleFile = 'DVTuples.root',
+        UserAlgorithms = [DecayTreeTuple('Dst2010ToD0ToKpipiTuple')],
+        CondDBtag = 'Sim08-20130503-vc-md100',
+        HistogramFile = 'DVHistos.root',
+        Lumi = True,
+        Simulation = True,
+        DDDBtag = 'Sim08-20130503')
+
+dv = DaVinci('DaVinci')
+
